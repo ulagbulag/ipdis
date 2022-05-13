472f2a7556384a76b30fa4947d7d99e5f29f14d8
Ho Kim <ho.kim@ulagbulag.io>
2022-05-13 05:14:57 +0000

    Add times in DynPath

--- api/postgres/migrations/2022-05-13-022727_create_dyn_path/up.sql
+++ api/postgres/migrations/2022-05-13-022727_create_dyn_path/up.sql
@@ -1,8 +1,10 @@
 -- Your SQL goes here
 CREATE TABLE dynpaths (
   id SERIAL PRIMARY KEY,
-  account VARCHAR(44) NOT NULL,  -- ED25519 PublicKey - base58 = 32 bytes
-  signature VARCHAR(88) NOT NULL,  -- ED25519 Signature - base58 = 64 bytes
+  account VARCHAR(44) NOT NULL,  -- METADATA, ED25519 PublicKey - base58 = 32 bytes
+  signature VARCHAR(88) NOT NULL,  -- METADATA, ED25519 Signature - base58 = 64 bytes
+  created_date DATETIME NOT NULL,  -- METADATA, 
+  expiration_date DATETIME NOT NULL,  -- METADATA, 
   name VARCHAR NOT NULL,
   path VARCHAR(44) NOT NULL,  -- SHA256 - base58 = 32 bytes
   len BIGINT NOT NULL
--- api/postgres/src/models/dyn_path.rs
+++ api/postgres/src/models/dyn_path.rs
@@ -1,8 +1,12 @@
+use ipis::core::chrono::{DateTime, Utc};
+
 #[derive(Queryable)]
 pub struct DynPaths {
     pub id: i32,
     pub account: String,
     pub signature: String,
+    pub created_date: DateTime<Utc>,
+    pub expiration_date: DateTime<Utc>,
     pub name: String,
     pub path: String,
     pub len: i64,
